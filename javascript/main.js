e6633763ce6d7084af741d5856940bb8a897eb3e
alex daniel baena yepez <alex.baena@est.iudigital.edu.co>
2023-01-08 22:30:59 -0500

    en la pagina de particles.js utilizan un procesador css para anidar selectores css y poder superponer el contenido a las particulas, voy a invertigar como loedo superponer el contenido si usar el preprocesador de css

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,20 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-//import RequestAPIBlockchair from "./RequestAPIBlockchair";
-
 particlesJS.load('particles-js', 'assets/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
+
+// Obtener informacion de la API
+
+var url = "https://api.blockchair.com/bitcoin/stats/0?key=G___oGohGjhTGHtVOx8mmlNZokAlz5b4";
+
+let Crypto = new XMLHttpRequest();
+Crypto.open('GET', url);
+Crypto.send();
+console.log(Crypto)
+
+Crypto.addEventListener('load', function (e) {
+  var data = e.target.response;
+  var response = JSON.parse(data);
+  console.log(response);
+});
+
