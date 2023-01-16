498ae6801a148730c1a06b1699286b640fe3fa51
alex daniel baena yepez <alex.baena@est.iudigital.edu.co>
2023-01-15 22:35:42 -0500

    tome los estilos de la tarjeta en la pagina oficial de particles y los agregue, la tarjeta quedo centrada

--- javascript/main.js
+++ javascript/main.js
@@ -16,5 +16,34 @@ Crypto.addEventListener('load', function (e) {
   var data = e.target.response;
   var response = JSON.parse(data);
   console.log(response);
-});
 
+  /* datos a mostrar
+
+  Nombre de la crypto
+  Valor en el mercado = market_price_usd
+  Market cap = market_cap_usd
+  circulacion = circulation
+  DIficultad de minado= difficulty
+  Numero de blokes = blocks
+  */
+
+  var mostrarDatos = response.data;
+  var mcap = mostrarDatos.best_block_hash;
+
+
+  var container = document.querySelector(".js-container");
+  container.innerHTML += "<p>hola \""+ mcap +"</p>";
+
+
+});
+{/*
+<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+*/}
