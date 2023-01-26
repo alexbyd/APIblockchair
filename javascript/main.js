a827d639848ec5d87b683532279fabf73f53c030
alex daniel baena yepez <alex.baena@est.iudigital.edu.co>
2023-01-25 20:14:22 -0500

    agregue la boxsahdou y modifiqeu el titulo

--- javascript/main.js
+++ javascript/main.js
@@ -17,18 +17,50 @@ Crypto.addEventListener('load', function (e) {
   var response = JSON.parse(data);
   console.log(response);
 
-  /* datos a mostrar
-
-  Nombre de la crypto
-  Valor en el mercado = market_price_usd
-  Market cap = market_cap_usd
-  circulacion = circulation
-  DIficultad de minado= difficulty
-  Numero de blokes = blocks
-  */
-
   var mostrarDatos = response.data;
 
   var container = document.querySelector(".js-container");
   container.innerHTML += "<p>hola \""+ mostrarDatos.blockchain_size +"</p>";
+
+  const market_price_usd = document.getElementById("market_price_usd");
+  market_price_usd.innerText = mostrarDatos.market_price_usd;
+
+  const market_cap_usd = document.getElementById("market_cap_usd");
+  market_cap_usd.innerText = mostrarDatos.market_cap_usd;
+
+  const difficulty = document.getElementById("difficulty");
+  difficulty.innerText = mostrarDatos.difficulty;
+
+  const blocks = document.getElementById("blocks");
+  blocks.innerText = mostrarDatos.blocks;
+
+  const blocks_24h = document.getElementById("blocks_24h");
+  blocks_24h.innerText = mostrarDatos.blocks_24h;
 });
+/*
+*
+* window.Blockchair = {
+        Host: "https://blockchair.com",
+        ApiHost: "https://api.blockchair.com",
+        CDN: "https://loutre.blockchair.io",
+        Page: "Dashboard",
+        PageProps: {},
+        ApiUrl: "/bitcoin/stats",
+        Hash: "",
+        Locale: "en",
+        chain: "bitcoin",
+        chains: ["bitcoin","ethereum","bnb","litecoin","cardano","ripple","polkadot","dogecoin","bitcoin-cash","stellar","monero","kusama","ecash","zcash","dash","mixin","groestlcoin"],
+        ticker: "BTC",
+        price_usd: 22642,
+        configChain: {"fraction":8,"has_mempool":true,"has_broadcast":true,"has_raw_tx":true,"bitcoin_like":true,"has_price_history":true,"has_suggested_fee":true,"has_wallet_statement":true,"colors":{"stroke":["#FFD600","#FF4F28","#FF0303"],"fill":["#FF8C21","#FFB421"]}},
+        blockchains: ["bitcoin","ethereum","bnb","litecoin","cardano","ripple","polkadot","dogecoin","bitcoin-cash","stellar","monero","kusama","ecash","zcash","dash","mixin","groestlcoin"],
+        locales: ["en","es","fr","it","nl","pt","ru","zh","fa","id","tr","ja","ko","de"],
+        Lang: {
+            'app.chain.bitcoin': "Bitcoin"
+        },
+        Currency: "USD"
+    };
+
+*
+*
+* */
